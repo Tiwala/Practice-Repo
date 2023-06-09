db8e9bb98789a8dd8e279fea805df87204dcfb74
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2023-06-09 13:24:47 -0400

    Finished 1.1

--- programming languages/java/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ programming languages/java/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println(" GGG    SSS ");
+         System.out.println("G   G  S   S");
+         System.out.println("G      S    ");
+         System.out.println("G GGG   SSS ");
+         System.out.println("G   G      S");
+         System.out.println("G   G  S   S");
+         System.out.println(" GGG    SSS ");
     }
 }
