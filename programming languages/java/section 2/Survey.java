e2900a309d83fa4588c3ad4bf76890540316d11f
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2023-06-13 18:45:02 -0400

    finished section 2

---
+++ programming languages/java/section 2/Survey.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Survey {
+    public static void main(String[] args) {
+
+        // USEFUL METHODS
+        // nextInt() - scans for the nearest Integer
+        // nextDouble() - self-explanatory
+        // nextLine() - Text
+
+        int counter = 0;
+
+        //*********PART A: PICKING UP THE USER'S ANSWERS*********
+        System.out.println("Welcome. Thank you for taking the survey");
+
+        // Creates a variable and new instance of scanner that can receive input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nWhat is your name?");
+        String name = scanner.nextLine();
+        counter++;
+
+        System.out.println("\nHow much money do you spend on coffee?");
+        double coffeePrice = scanner.nextDouble();
+        counter++;
+
+        System.out.println("\nHow much money do you spend on fast food?");
+        double foodPrice = scanner.nextDouble();
+
+        System.out.println("\nHow many times a week do you buy coffee?");
+        int coffeeAmount = scanner.nextInt();
+        counter++;
+
+        System.out.println("\nHow many times a week do you buy fast food?");
+        int foodAmount = scanner.nextInt();
+        counter++;
+
+        // Always close Scanner once done, otherwise, you're gonna get a resource leak
+        scanner.close();
+
+        //*********PART B: RESPONDING TO THE USER**********
+        System.out.println("Thank you " + name + " for answering all " + counter + " questions");
+        System.out.println("Weekly, you spend $" + ((double)coffeePrice * coffeeAmount) + " on coffee");
+        System.out.println("Weekly, you spend $" + ((double)foodPrice * foodAmount) + " on food");
+
+    }
+}
